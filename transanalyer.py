5c22bbc4038b664eeabf1b0717346711ed41fb12
Rehmana <rehmana.younis1@gmail.com>
2024-10-01 18:24:48 -0400

    Complete Project

--- transanalyer.py
+++ transanalyer.py
@@ -1,26 +1,21 @@
 import warnings 
 warnings.filterwarnings('ignore')
 
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings, OpenAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_community.document_loaders import PyPDFLoader, TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from dotenv import load_dotenv
-import dataclasses
-from dataclasses import dataclass
-from typing import List, Dict
-from langchain_community import output_parsers
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.pydantic_v1 import BaseModel, Field
 from youtube_transcript_api import YouTubeTranscriptApi
-import os
+from langchain_core.prompts import ChatPromptTemplate
+from dataclasses import dataclass
+from typing import List, Dict
+import dataclasses, os
+
+from dotenv import load_dotenv
 load_dotenv()
 
 class ReviewClassification(BaseModel):
     """Pydantic model to get the sentiment of the excerpt"""
     sentiment: str = Field(description='The sentiment of the text', enum=["positive", "negative", "neutral"])
-    # language: str= Field (..., enum=["Roman Urdu", "Spanish", "English"])
-    # topic: str=Field(description="Holds information on the transcript ")
             
 @dataclass
 class Transanalyer:
@@ -40,27 +35,26 @@ class Transanalyer:
         self.url = url
         try:
             video_id=url.split("=")[1]
-        
             transcript_text=YouTubeTranscriptApi.get_transcript(video_id)
-
             transcript = ""
             for i in transcript_text:
                 transcript += " " + i["text"]
             self.transcript = transcript
-            with open (f'{video_id}.txt', 'w', encoding='utf-8') as file:
+            os.makedirs('transcripts', exist_ok=True)
+            with open (f'transcripts/{video_id}.txt', 'w', encoding='utf-8') as file:
                 file.write(self.transcript)
                 
             return self.transcript
 
-        except Exception as e:
-            raise e
+        except Exception as error:
+            raise error
     
     def get_chunks(self):   
         """
         Setting the self.chunks 
         """
         video_id=self.url.split("=")[1]
-        with open(f'{video_id}.txt', 'r', encoding='UTF-8') as file:
+        with open(f'transcripts/{video_id}.txt', 'r', encoding='UTF-8') as file:
             text_content = file.read()
 
         splitter = RecursiveCharacterTextSplitter(
@@ -85,19 +79,18 @@ class Transanalyer:
         model = ChatGoogleGenerativeAI(model='gemini-1.5-flash').with_structured_output(ReviewClassification)
         chain = prompt | model 
         resp = [chain.invoke({"chunk": chunk}) for chunk in self.chunks]        
-        for x, passage in zip(resp, self.chunks):
+        for x in resp:
             if x and (x.sentiment.lower() != ''):
                 self.res[x.sentiment.lower()] = self.res.get(x.sentiment, 0) + 1
-                print(passage, x.sentiment)
         
     
     def pipeline(self, url):
         """
         Complete pipeline to do transcript analysis
-        @params-input: 
-            url: Youtube video URL
-        @params-return:
-            res: sentiment dictionary
+        @params-url: str 
+            Youtube video URL
+        @params-return: Dict[str, int]
+            sentiment dictionary
         """
         self.get_transcription(url)
         self.get_chunks()
